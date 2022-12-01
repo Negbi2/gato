904aebc287ec3c3f6810ea804275054af20b226f
Negbi2 <itzhakn@protonmail.com>
2022-12-01 23:56:50 +0200

    cant overlap used spaces

--- gato/src/TickTack.java
+++ gato/src/TickTack.java
@@ -7,17 +7,25 @@ public class TickTack {
                         {"-","-","-","-","-"},
                         {" ","|"," ","|"," "}};
 
+    //game objects
     int xCoor = 0, yCoor = 0, counter = 1;
+
+    //win conditions
     int rOne = 0,rTwo = 0, rThree = 0;
     int cOne = 0, cTwo = 0, cThree = 0;
     int dOne = 0, dTwo = 0;
-    String x = "";
+
+    //check if a slot is ocupied
+    boolean ocupied11 = false,ocupied12 = false,ocupied13 = false;
+    boolean ocupied21 = false,ocupied22 = false,ocupied23 = false;
+    boolean ocupied31 = false,ocupied32 = false,ocupied33 = false;
+
+    //coordinate input
+    String coordinates = "";
     Scanner in = new Scanner(System.in);
 
-
-
     public void play() {
-        while (!x.equals("win")){
+        while (!coordinates.equals("win")){
             for (int fila = 0; fila < 5; fila++) {
                 for (int columna = 0; columna < 5; columna++) {
                     System.out.print(tickTackToe[fila][columna]);
@@ -26,55 +34,100 @@ public class TickTack {
             }
             boolean checker = false;
             while (!checker) {
-                x = in.next();
-                switch (x) {
+                coordinates = in.next();
+                switch (coordinates) {
                     //first row
                     case "1,1" -> {
-                        xCoor = 0;
-                        yCoor = 0;
-                        checker = true;
+                        if(!ocupied11){
+                            xCoor = 0;
+                            yCoor = 0;
+                            checker = true;
+                            ocupied11 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     case "1,2" -> {
-                        xCoor = 0;
-                        yCoor = 2;
-                        checker = true;
+                        if(!ocupied12){
+                            xCoor = 2;
+                            yCoor = 0;
+                            checker = true;
+                            ocupied12 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                      case "1,3" -> {
-                        xCoor = 0;
-                        yCoor = 4;
-                        checker = true;
+                        if(!ocupied13){
+                            xCoor = 4;
+                            yCoor = 0;
+                            checker = true;
+                            ocupied13 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     //second row
                     case "2,1" -> {
-                        xCoor = 2;
-                        yCoor = 0;
-                        checker = true;
+                         if(!ocupied21){
+                            xCoor = 0;
+                            yCoor = 2;
+                            checker = true;
+                            ocupied21 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     case "2,2" -> {
-                        xCoor = 2;
-                        yCoor = 2;
-                        checker = true;
+                        if(!ocupied22){
+                            xCoor = 2;
+                            yCoor = 2;
+                            checker = true;
+                            ocupied22 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     case "2,3" -> {
-                        xCoor = 2;
-                        yCoor = 4;
-                        checker = true;
+                        if(!ocupied23){
+                            xCoor = 4;
+                            yCoor = 2;
+                            checker = true;
+                            ocupied23 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     //third row
                     case "3,1" -> {
-                        xCoor = 4;
-                        yCoor = 0;
-                        checker = true;
+                        if(!ocupied31){
+                            xCoor = 0;
+                            yCoor = 4;
+                            checker = true;
+                            ocupied31 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     case "3,2" -> {
-                        xCoor = 4;
-                        yCoor = 2;
-                        checker = true;
+                        if(!ocupied32){
+                            xCoor = 2;
+                            yCoor = 4;
+                            checker = true;
+                            ocupied32 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     case "3,3" -> {
-                        xCoor = 4;
-                        yCoor = 4;
-                        checker = true;
+                        if(!ocupied33){
+                            xCoor = 4;
+                            yCoor = 4;
+                            checker = true;
+                            ocupied33 = true;
+                        }else{
+                            System.out.println("ocupied");
+                        }
                     }
                     default -> System.out.println("that is not a valid option");
                 }
