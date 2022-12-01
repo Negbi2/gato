aba27903b51666fe72d1593325bb3613f812a4b7
Negbi2 <itzhakn@protonmail.com>
2022-12-01 22:31:56 +0200

    more intuitive input system
    a more intuituve input system along with starting the code for the win screen

--- gato/src/TickTack.java
+++ gato/src/TickTack.java
@@ -7,13 +7,17 @@ public class TickTack {
                         {"-","-","-","-","-"},
                         {" ","|"," ","|"," "}};
 
-    int xCoor = 0,yCoor = 0, counter = 1;
+    int xCoor = 0, yCoor = 0, counter = 1;
+    int rOne = 0,rTwo = 0, rThree = 0;
+    int cOne = 0, cTwo = 0, cThree = 0;
+    int dOne = 0, dTwo = 0;
     String x = "";
-    String y = "";
     Scanner in = new Scanner(System.in);
 
+
+
     public void play() {
-        while (!x.equals("win") && !y.equals("win")) {
+        while (!x.equals("win")){
             for (int fila = 0; fila < 5; fila++) {
                 for (int columna = 0; columna < 5; columna++) {
                     System.out.print(tickTackToe[fila][columna]);
@@ -24,47 +28,64 @@ public class TickTack {
             while (!checker) {
                 x = in.next();
                 switch (x) {
-                    case "1" -> {
+                    //first row
+                    case "1,1" -> {
                         xCoor = 0;
-                        checker = true;
-                    }
-                    case "2" -> {
-                        xCoor = 2;
-                        checker = true;
-                    }
-                    case "3" -> {
-                        xCoor = 4;
-                        checker = true;
-                    }
-                    case "win" -> System.out.println("congratulations you won");
-                    default -> System.out.println("that is not a valid option");
-                }
-            }
-            checker = false;
-            while (!checker) {
-                y = in.next();
-                switch (y) {
-                    case "1" -> {
                         yCoor = 0;
                         checker = true;
                     }
-                    case "2" -> {
+                    case "1,2" -> {
+                        xCoor = 0;
                         yCoor = 2;
                         checker = true;
                     }
-                    case "3" -> {
+                     case "1,3" -> {
+                        xCoor = 0;
                         yCoor = 4;
                         checker = true;
                     }
-                    case "win" -> System.out.println("congratulations you won");
+                    //second row
+                    case "2,1" -> {
+                        xCoor = 2;
+                        yCoor = 0;
+                        checker = true;
+                    }
+                    case "2,2" -> {
+                        xCoor = 2;
+                        yCoor = 2;
+                        checker = true;
+                    }
+                    case "2,3" -> {
+                        xCoor = 2;
+                        yCoor = 4;
+                        checker = true;
+                    }
+                    //third row
+                    case "3,1" -> {
+                        xCoor = 4;
+                        yCoor = 0;
+                        checker = true;
+                    }
+                    case "3,2" -> {
+                        xCoor = 4;
+                        yCoor = 2;
+                        checker = true;
+                    }
+                    case "3,3" -> {
+                        xCoor = 4;
+                        yCoor = 4;
+                        checker = true;
+                    }
                     default -> System.out.println("that is not a valid option");
                 }
             }
+
+            //check whose turn is it and alocate the piece
             counter ++;
-            if (counter%2 == 0) {
+            if (counter % 2 == 0){
                 tickTackToe[yCoor][xCoor] = "x";
             }else{
-                tickTackToe[yCoor][xCoor] = "0";
+                tickTackToe[yCoor][xCoor] = "o";
             }
         }
     }
